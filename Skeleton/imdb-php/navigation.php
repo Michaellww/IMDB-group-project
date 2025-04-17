8af854f07925dfccf85a675b0fba5bfd655f9bbf
tem68 <tem68@uclive.ac.nz>
2025-04-17 14:18:52 +1200

    Removed description and image of Yoda but retained the search bar. Changed yellow BG to black BG for the navigation bar. Improved nav buttons with minimizing screen (not yet fully functional yet). Drop-down button appears for small screens.

--- Skeleton/imdb-php/navigation.php
+++ Skeleton/imdb-php/navigation.php
@@ -1,11 +1,30 @@
-<nav class="navbar navbar-expand-lg bg-yellow-500">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
     <div class="container-fluid">
-        <a class="navbar-brand p-2" href="index.php">IMDB 2</a>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link p-2" href="titles.php">Titles</a>
-                <a class="nav-link p-2" href="#">People</a>
-            </div>
+        <a class="navbar-brand" href="#">IMDB</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="movies.php">Movies</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="series.php">Series</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="people.php">People</a>
+                </li>
+            </ul>
+
+            <span class="navbar-text text-light">
+        Welcome to the Movie DB
+      </span>
         </div>
     </div>
 </nav>
