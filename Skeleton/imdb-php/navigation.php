1b4992c5157898732fb52758820ab21747f8e2b3
tem68 <tem68@uclive.ac.nz>
2025-04-27 19:30:14 +1200

    Single page layout responsive. tested with dummy text without any problems. Also Back on top page responsive website also works normally. Changed nav titles for sections.

--- Skeleton/imdb-php/navigation.php
+++ Skeleton/imdb-php/navigation.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top w-100">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">IMDB Project</a>
+        <a class="navbar-brand" href="#">IMDB Project</a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,20 +10,23 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    <a class="nav-link active" href="#popular-movies">Movies</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="movies.php">Movies</a>
+                    <a class="nav-link" href="#popular-actors">Actors</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Series</a>
+                    <a class="nav-link" href="#birthday-celebs">Birthdays</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">People</a>
+                    <a class="nav-link" href="#top-rated">Top Rated</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#popular-characters">Characters</a>
                 </li>
             </ul>
 
-            <!-- 🔍 Search form -->
+            <!-- Search form -->
             <form class="d-flex" role="search">
                 <input id="search-input" class="form-control me-2" type="search" placeholder="Search..." aria-label="Search">
                 <button id="search-button" class="btn btn-warning" type="submit">Search</button>
