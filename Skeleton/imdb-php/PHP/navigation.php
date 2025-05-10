cabe9b48d95f5c905e4623b2849f1bb65cfc560e
tem68 <tem68@uclive.ac.nz>
2025-05-10 20:15:12 +1200

    Arrow hover on cards is good. gonna do them on the rest. maybe i can do the reactjs btw.

--- Skeleton/imdb-php/PHP/navigation.php
+++ Skeleton/imdb-php/PHP/navigation.php
@@ -8,15 +8,12 @@
         </button>
 
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav mx-auto text-center mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link" href="#popular-movies">Movies</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#popular-actors">Actors</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#birthday-celebs">Birthdays</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#top-rated">Top Rated</a>
