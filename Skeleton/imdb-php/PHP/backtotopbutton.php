c1d52e03f550458ac78775a98896da586fec85c8
tem68 <tem68@uclive.ac.nz>
2025-05-14 19:42:24 +1200

    Well good luck to us. Added l ogin for the wishlist of the movie.

---
+++ Skeleton/imdb-php/PHP/backtotopbutton.php
@@ -0,0 +1,4 @@
+<button id="backToTopBtn" class="btn btn-primary position-fixed"
+        style="top: 50px; left: 50%; transform: translateX(-50%); display: none; z-index: 1050;">
+    ↑ Back to Top
+</button>
