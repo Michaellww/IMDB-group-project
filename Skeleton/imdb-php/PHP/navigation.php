9557423f0027f4fb284101523305fc53b06c34b3
tem68 <tem68@uclive.ac.nz>
2025-05-14 19:42:58 +1200

    Well good luck to us. Added l ogin for the wishlist of the movie.

--- Skeleton/imdb-php/PHP/navigation.php
+++ Skeleton/imdb-php/PHP/navigation.php
@@ -1,3 +1,10 @@
+<?php
+#session_start();
+$loggedIn = isset($_SESSION['user']);
+$username = $loggedIn ? htmlspecialchars($_SESSION['user']['email']) : "Not logged in";
+?>
+
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark w-1000">
     <div class="container px-5">
         <a class="navbar-brand" href="#">IMDB Project</a>
@@ -22,6 +29,16 @@
                     <a class="nav-link" href="#popular-characters">Characters</a>
                 </li>
             </ul>
+
+            <!-- Profile button and login status -->
+            <div class="d-flex align-items-center gap-3">
+                <button class="btn btn-outline-light d-flex align-items-center"
+                        onclick="<?= $loggedIn ? 'window.location.href=\'profile.php\'' : 'promptLogin()' ?>"
+                        style="border-radius: 50%;">
+                <img src="../images/avatarpl.png" alt="Profile" style="width: 32px; height: 32px; border-radius: 50%;">
+                </button>
+                <span class="text-white small"><?= $username ?></span>
+            </div>
 
             <form class="d-flex position-relative" role="search" action="search.php" method="get">
                 <input name="query" id="searchInput" class="form-control me-2" type="search" placeholder="Search..." aria-label="Search">
