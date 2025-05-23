b8d2a1e4103dcf4efe94f2c90fafaac539101883
tem68 <tem68@uclive.ac.nz>
2025-05-23 23:17:27 +1200

    bye

--- INFO263 FINAL/imdb-skeleton/imdb-php/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-define('TMDB_API_KEY', 'a89d53c18c2585eff159a3ee65bed7db');
-?>
--- INFO263 FINAL/imdb-skeleton/imdb-php/cookiepopup.php
+++
@@ -1,9 +0,0 @@
-<div id="cookieConsent" class="position-fixed bottom-0 start-50 translate-middle-x bg-dark text-white p-3 rounded shadow-lg"
-     style="z-index: 9999; display: none; max-width: 90%; width: 400px;">
-    <div class="d-flex justify-content-between align-items-center">
-        <div>
-            🍪 We use cookies to enhance your experience. By continuing to visit this site, you agree to our use of cookies.
-        </div>
-        <button class="btn btn-light btn-sm ms-3" onclick="acceptCookies()">OK</button>
-    </div>
-</div>
